80db46f1bb0581ce12b9ce7b72d13afc8714148c
noahpatterson <n@noahpatterson.me>
2017-01-05 09:28:25 -0700

    register and schedule local notification

--- LocalNotifications/ViewController.swift
+++ LocalNotifications/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +24,38 @@ class ViewController: UIViewController {
     }
 
 
+    func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) {
+            (granted, error) in
+            if granted {
+                print("yay")
+            } else {
+                print("nooooo")
+            }
+        }
+    }
+    
+    func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body  = "body"
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData" : "fizzbuzz"]
+        content.sound = UNNotificationSound.default()
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        center.add(request)
+    }
 }
 
